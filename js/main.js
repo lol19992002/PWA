f73dac69a0d47bdfec59ebc0319e1fa86fe24a4f
Mateusz-Melson <matmels@wp.pl>
2025-01-28 17:57:28 +0100

    app
    .

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('./sw.js')
+        .then(() => console.log('Service Worker registered'))
+        .catch(err => console.error('Service Worker registration failed:', err));
+    }
+  };
+  
